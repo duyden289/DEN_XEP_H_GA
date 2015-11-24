b67b39c75b247c728dad1e2b367bd6bedc6bd18b
Nguyen Huu Den <nguyenhuuden289@gmail.com>
2015-11-24 23:49:37 +0700

    Add new Project Project

---
+++ TomCandy_Demo/TomCandy_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TomCandy_Demo
+//
+//  Created by admin on 11/24/15.
+//  Copyright © 2015 nguyenhuuden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TomCandy_Demo/TomCandy_Demo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TomCandy_Demo
+//
+
+//  Copyright (c) 2015 nguyenhuuden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
